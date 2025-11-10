eb9364850064bac7572fb0bfb8cf342cc8824096
Yiğit Türk <yigit.turk@ug.bilkent.edu.tr>
2025-11-10 21:40:09 +0300

    The class Test1 is implemented

---
+++ Test1.java
@@ -0,0 +1,7 @@
+public class Test1
+{
+    public static void func(int x)
+    {
+        System.out.println("The inputted number is " + x);
+    }    
+}
--- WorkSpace.java
+++ WorkSpace.java
@@ -5,5 +5,6 @@ public class WorkSpace
   public static void main( String[] args ) 
   {
     System.out.println("Hello World");
+    Test1.func(10);
   }
 }
